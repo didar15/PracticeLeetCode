9680aead251f70dc69c6f13029a6fde5fbd8c8c4
didar15 <didar1513@gmail.com>
2020-10-17 01:26:16 -0400

    LC347_Top_K_Frequent_ElementsMD

---
+++ src/LC3/LC347_Top_K_Frequent_ElementsMD.java
@@ -0,0 +1,39 @@
+package LC3;
+
+import java.util.*;
+
+public class LC347_Top_K_Frequent_ElementsMD {
+    public static int[] topKFrequent(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        //Create a frequency table
+        for (int i = 0; i < nums.length; i++) {
+            int count=  map.getOrDefault(nums[i],0)+1;
+            map.put(nums[i],count);
+        }
+        //Create a Max Heap and add all the distinct elements
+        //will use map values to sort in heap in decending order
+        Queue<Integer> q = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
+
+        //add all key values to heap
+        //inputing keys which priority queue sorts by decending values
+        for (int key: map.keySet()) {
+            q.add(key);
+            if (q.size() > k) q.poll();
+        }
+        int [] res= new int[k];
+        //Poll top k frequent elements off the Heap
+        for (int i = 0; i < k; i++) {
+            res[i]=q.poll();
+        }
+
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        int [] a={1,1,1,2,2,3};
+        System.out.println(Arrays.toString(topKFrequent(a, 2)));
+    }
+}
