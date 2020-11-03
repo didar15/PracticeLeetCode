0772b742d22a040690572aa41ba301c9d25934eb
didar15 <didar1513@gmail.com>
2020-11-03 16:50:24 -0500

    LC242_Valid_AnagramEZ

---
+++ src/LC3/LC242_Valid_AnagramEZ.java
@@ -0,0 +1,28 @@
+package LC3;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class LC242_Valid_AnagramEZ {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] counter = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
+        }
+        for (int count : counter) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isAnagram("anagram","nagaram"));
+    }
+}
